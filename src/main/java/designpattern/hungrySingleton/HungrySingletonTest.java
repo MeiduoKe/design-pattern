edd9ac0533014be069779f83dfc255152f61f500
kemeiduo@ctesi.com <Ctesi@1234>
2023-02-02 17:53:22 +0800

    初始化

--- src/main/java/designpattern/hungrySingleton/HungrySingletonTest.java
+++ src/main/java/designpattern/hungrySingleton/HungrySingletonTest.java
@@ -8,4 +8,24 @@ package designpattern.hungrySingleton;
  * @Version 1.0
  */
 public class HungrySingletonTest {
+    public static void main(String[] args) {
+        HungrySingleton hungrySingleton = HungrySingleton.getInstance();
+        HungrySingleton hungrySingleton1 = HungrySingleton.getInstance();
+        System.out.println(hungrySingleton1 == hungrySingleton);
+
+    }
+}
+
+
+class HungrySingleton {
+
+    private static HungrySingleton instance = new HungrySingleton();
+
+    // 私有化构造器
+    private HungrySingleton() {
+    }
+
+    public static HungrySingleton getInstance() {
+        return instance;
+    }
 }
